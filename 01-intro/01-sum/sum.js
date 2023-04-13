fa7eac432ea65d0fe420fc4356579812111b52ac
Daniil Maltsev <daniil.malcev@inbox.ru>
2023-04-13 00:16:53 -0400

    solution task-1

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,10 @@
 function sum(a, b) {
-  /* ваш код */
+
+  if (typeof a === 'number' & typeof b === 'number') {
+    return a + b;
+  } else {
+    throw new TypeError('Not a Number')
+  }
 }
 
 module.exports = sum;
